d8b007cf934329f21dff7a7b9ca6f4abbeda476a
Aryan-1212 <aryanparvani23@gmail.com>
2024-08-21 14:43:31 +0530

    appwrite-configuration

---
+++ src/conf/Conf.js
@@ -0,0 +1,9 @@
+const Conf = {
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default Conf
